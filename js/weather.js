1e8f3f1e36fd27e8a42155e40e5db14f852e170e
Matthias Pinnewaert <pinnewaert.matthias@gmail.com>
2016-04-20 17:23:52 +0200

    Added files via upload

--- js/weather.js
+++ js/weather.js
@@ -9,6 +9,7 @@ var lat = 50.848921, lon = 4.351484; //Brussel
 var temp = 0, loc = '';
 
 $(document).ready(function() {
+	getLocation();
 	getWeather();
 });	
 
@@ -22,7 +23,6 @@ function getLocation() {
 }
 
 function getWeather() {
-	getLocation();
 	var locationUrl = url + 'lat=' + lat + '&lon=' + lon + '&APPID=' + APIKEY;
 	$.getJSON(locationUrl, function(laWeer){
 		var iconUrl = 'http://openweathermap.org/img/w/' + laWeer.weather[0].icon + '.png';
