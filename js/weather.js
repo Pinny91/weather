4ef178ce24ea6bffc8a7d8b5845b3b0fbf7aefc5
Matthias Pinnewaert <pinnewaert.matthias@gmail.com>
2016-04-21 15:56:05 +0200

    Added files via upload

--- js/weather.js
+++ js/weather.js
@@ -2,15 +2,17 @@
 var APIKEY = '061f24cf3cde2f60644a8240302983f2';
 
 var url = 'http://api.openweathermap.org/data/2.5/weather?'
-var cb = "&callback=JSON_CALLBACK";
+
+var result = null; //DATA VAN HET WEER
+var iconUrl = "";
+
 
 
 var lat = 50.848921, lon = 4.351484; //Brussel
 var temp = 0, loc = '';
 
 $(document).ready(function() {
-	getLocation();
-	getWeather();
+	getWeather();	
 });	
 
 function getLocation() {
@@ -23,21 +25,30 @@ function getLocation() {
 }
 
 function getWeather() {
+	getLocation();
 	var locationUrl = url + 'lat=' + window.lat + '&lon=' + window.lon + '&APPID=' + APIKEY;
-	$.getJSON(locationUrl, function(laWeer){
-		var iconUrl = 'http://openweathermap.org/img/w/' + laWeer.weather[0].icon + '.png';
-		temp = Math.round((laWeer.main.temp - 273)*100)/100;
-		loc = laWeer.name;
-			
-		var tempHtml = $('#temperature');
-		var imgWeather = $('#icon');
-		var appWeather = $('#weather-block');
+	$.getJSON(locationUrl, function(data) {
+		changeHTML(data);
+		console.log( "success" );
+	})
+}
+
+function changeHTML(laWeer){
+		window.iconUrl = 'http://openweathermap.org/img/w/' + laWeer.weather[0].icon + '.png';
+		window.temp = Math.round((laWeer.main.temp - 273)*100)/100;
+		window.loc = laWeer.name;
 		
-		tempHtml.html(
-			'<p id="temp">' + '<img src="' + iconUrl + '">' + temp + '°C in '+ loc + '</p>'
-		);
+		conversionCF();
 		//imgWeather.html('<img src="' + iconUrl + '">')
-		
+			
+			var tempHtml = $('#temperature');
+			var imgWeather = $('#icon');
+			var appWeather = $('#weather-block');
+			var conversion = $('#convert');
+	
+		tempHtml.html(
+			'<p>' + '<img src="' + iconUrl + '">' + '<i id="temp">' + window.temp + '°C </i> in '+ loc + '</p>'
+		);
 		if(laWeer.weather[0].id > 900) appWeather.css('background-color', '#161A44');
 		else if(laWeer.weather[0].id == 800) appWeather.css('background-color', '#E5FD00');
 		else if(laWeer.weather[0].id == 801 ) appWeather.css('background-color', '#DDF35A');
@@ -51,5 +62,23 @@ function getWeather() {
 		else if(laWeer.weather[0].id > 501) appWeather.css('background-color', '#4879CE');
 		else if(laWeer.weather[0].id == 500) appWeather.css('background-color', '#2265DA');
 		else appWeather.css('background-color', '#0CB90F');	
+}
+
+function conversionCF() {
+	var conversion = $('#convert');
+	conversion.on('click' ,function(){
+			var tempChange = $('#temp');
+			if(conversion.hasClass('cel')) {
+				window.temp = temp * 9 / 5 + 32;
+				conversion.addClass('far');
+				conversion.removeClass('cel');
+				tempChange.html(window.temp + '°F');
+			} else { 
+				window.temp = (temp-32) / 9 * 5;
+				conversion.addClass('cel');
+				conversion.removeClass('far');
+				tempChange.html(window.temp + '°C');
+			}
+	console.log(temp);
 	});
 }
